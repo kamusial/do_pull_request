ae74514ece07e91b67a0ee01928908699705f04d
vdi-student <vdi-student@EDU-WSB.local>
2022-03-06 10:56:00 +0100

    Try this

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 path = 'C://Users//kamusial//Desktop//rolling_stones.txt'
+from ile_a import *
+from usun_puste import *
+
 with open (path, 'r') as file:
   content = file.readlines()
   content2 = file.read()
@@ -7,7 +10,7 @@ with open (path, 'r') as file:
 content_bez_pustych = usun_puste(content)
 
 #funkcja licząca ilość wystapień litery "a"     "ile_a"
-print('Litera "a" wystepuje ', ile_a(content),' razy'
+print('Litera "a" wystepuje ', ile_a(content2),' razy'
       
 #funkcja licząca ilość wystapień wskazanej litery     "ile"
 print('Litera ' ,x, ' wystepuje ', ile(content, x),' razy'     
---
+++ ile_a.py
@@ -0,0 +1,3 @@
+def ile_a(content2):
+    finda = content2.count("a")
+    return finda
